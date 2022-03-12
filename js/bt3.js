03ea19fac99b00f00fd64cb2d79212dcde0a23c3
viettoanle2304 <vietoan230499@gmail.com>
2022-03-12 11:37:47 +0700

    bt3-complete

---
+++ js/bt3.js
@@ -0,0 +1,20 @@
+const countOddEven = () => {
+  const numArr = document
+    .getElementById("bt3__duLieu")
+    .value.split(",")
+    .map(Number);
+
+  console.log(numArr);
+
+  const countOdd = numArr.filter((val) => val % 2 == 1).length;
+  const countEven = numArr.filter((val) => val % 2 == 0).length;
+
+  console.log(`Lẻ: ${countOdd} \n Chẵn: ${countEven}`);
+
+  document.getElementById(
+    "bt3__ketQua--odd"
+  ).innerHTML = `${countOdd} số lẻ và `;
+  document.getElementById(
+    "bt3__ketQua--even"
+  ).innerHTML = `${countEven} số chẵn`;
+};
