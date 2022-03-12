ba84b3872c8f2b90add75a64921dcb39f9a9ab99
viettoanle2304 <vietoan230499@gmail.com>
2022-03-12 11:12:48 +0700

    bt2-complete

---
+++ js/bt2.js
@@ -0,0 +1,27 @@
+document.addEventListener("click", (e) => {
+  const element = e.target;
+
+  if (element.tagName == "BUTTON") {
+    const member = element.value;
+    console.log(member);
+
+    switch (member) {
+      case "B":
+        document.getElementById("bt2__ketQua").innerHTML =
+          "Bố đang sử dụng máy tính. Chào Bố!";
+        break;
+      case "M":
+        document.getElementById("bt2__ketQua").innerHTML =
+          "Mẹ đang sử dụng máy tính. Chào Mẹ!";
+        break;
+      case "A":
+        document.getElementById("bt2__ketQua").innerHTML =
+          "Anh trai đang sử dụng máy tính. Chào Anh trai!";
+        break;
+      case "E":
+        document.getElementById("bt2__ketQua").innerHTML =
+          "Em gái đang sử dụng máy tính. Chào Em gái!";
+        break;
+    }
+  }
+});
